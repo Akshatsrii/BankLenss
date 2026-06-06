713682aab6c634ad6806ac4fa0eec8c6745a09ff
Akshat Srivastava <chikusrivastava535@gmail.com>
2026-06-06 22:11:15 +0530

    Functions folder changes

--- Admin/src/routes/AppRoutes.jsx
+++ Admin/src/routes/AppRoutes.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Upload from "../pages/Upload";
 import Transactions from "../pages/Transactions";
+import Analytics from "../pages/Analytics";
+import Export from "../pages/Export";
 
 export default function AppRoutes() {
   return (
@@ -8,6 +10,8 @@ export default function AppRoutes() {
       <Route path="/" element={<Navigate to="/upload" replace />} />
       <Route path="/upload" element={<Upload />} />
       <Route path="/transactions" element={<Transactions />} />
+      <Route path="/analytics" element={<Analytics />} />
+      <Route path="/export" element={<Export />} />
     </Routes>
   );
 }
