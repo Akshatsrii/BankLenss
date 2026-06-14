a75a456da9fad711e8342ab714bf412e3d244676
Akshat Srivastava <chikusrivastava535@gmail.com>
2026-06-14 15:50:30 +0530

    Introduce protected routes and auth hooks for admin

--- Admin/src/routes/AppRoutes.jsx
+++ Admin/src/routes/AppRoutes.jsx
@@ -1,17 +1,32 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import Upload from "../pages/Upload";
+import ProtectedRoute from "../components/ProtectedRoute";
+import Login        from "../pages/Login";
+import Signup       from "../pages/Signup";
+import Upload       from "../pages/Upload";
 import Transactions from "../pages/Transactions";
-import Analytics from "../pages/Analytics";
-import Export from "../pages/Export";
+import Statements   from "../pages/Statements";
 
 export default function AppRoutes() {
   return (
     <Routes>
+      {/* Public routes */}
+      <Route path="/login"  element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+
+      {/* Protected routes */}
+      <Route path="/upload" element={
+        <ProtectedRoute><Upload /></ProtectedRoute>
+      } />
+      <Route path="/transactions" element={
+        <ProtectedRoute><Transactions /></ProtectedRoute>
+      } />
+      <Route path="/statements" element={
+        <ProtectedRoute><Statements /></ProtectedRoute>
+      } />
+
+      {/* Default redirect */}
       <Route path="/" element={<Navigate to="/upload" replace />} />
-      <Route path="/upload" element={<Upload />} />
-      <Route path="/transactions" element={<Transactions />} />
-      <Route path="/analytics" element={<Analytics />} />
-      <Route path="/export" element={<Export />} />
+      <Route path="*" element={<Navigate to="/upload" replace />} />
     </Routes>
   );
 }
